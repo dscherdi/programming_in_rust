dc70661bccc09086b6cd10e901119ce6dbd0dfd0
dscherdil <dscherdil@gmail.com>
2021-10-04 15:52:39 +0200

    Add files

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+use rocket::{get, routes};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}
